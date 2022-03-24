238efe13697e52ed7cc9ab8baacca85e86dec0b6
krzysztoftalarczyk <tal03@wp.pl>
2022-03-24 16:54:51 +0100

    first commit

---
+++ src/components/gallery.jsx
@@ -0,0 +1,32 @@
+
+export const Gallery = (props) => {
+  return (
+    <div id='portfolio' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Współpraca</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p>
+        </div>
+        <div class="row align-items-md-stretch">
+      <div class="col-md-6">
+        <div class="h-100 p-5 text-white bg-dark rounded-3">
+          <h2>Lorem Ipsum</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet est in libero hendrerit sollicitudin. Mauris sit amet congue purus. Suspendisse quis massa malesuada, volutpat magna et, ultrices enim. Duis neque felis, tristique a tincidunt nec, lacinia vel justo. In dictum suscipit feugiat. Nam volutpat fringilla lorem, in efficitur enim finibus vel. Aenean dictum vitae mauris sit amet posuere. Donec tincidunt elit risus, eu cursus tellus mattis at. Nullam non felis lectus.</p>
+          <button class="btn btn-outline-light" type="button">Example button</button>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="h-100 p-5 bg-light border rounded-3">
+          <h2>Lorem Ipsum</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet est in libero hendrerit sollicitudin. Mauris sit amet congue purus. Suspendisse quis massa malesuada, volutpat magna et, ultrices enim. Duis neque felis, tristique a tincidunt nec, lacinia vel justo. In dictum suscipit feugiat. Nam volutpat fringilla lorem, in efficitur enim finibus vel. Aenean dictum vitae mauris sit amet posuere. Donec tincidunt elit risus, eu cursus tellus mattis at. Nullam non felis lectus.</p>
+          <button class="btn btn-outline-secondary" type="button">Example button</button>
+        </div>
+      </div>
+    </div>
+      </div>
+    </div>
+  )
+}
